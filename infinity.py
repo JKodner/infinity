06591d0aa2dc2d9a5608f5fe105e5c3ce13bf94c
Jacob Kodner <jake@kodner.net>
2014-02-20 12:44:59 -0800

    Added Step Parameter

--- infinity.py
+++ infinity.py
@@ -1,12 +1,18 @@
-def inf(mode="+", start=0):
+def inf(mode="+", start=0, step=1):
 	"""Returns a Generator with an infinite amount of positive or negative numbers.
-	There are two parameters that one can input. The first is the positive or negative mode +/-.
-	The second is the starting point of the infinity generator."""
-	if mode not in ["+", "-"]:
+	There are three parameters that one can input. The first is the positive or negative mode +/-.
+	The second is the starting point of the infinity generator.
+	The third is the incremtor (step) for the generator."""
+	if mode in ["+", "-"]:
+		if isinstance(start, int) and isinstance(step, int):
+			while True:
+				if mode == "+":
+					start += step
+				elif mode == "-":
+					start -= step
+				yield start
+		else:
+			raise ValueError("Start/Step must be integers.")
+	else:
 		raise ValueError("Input must be '+' or '-'")
-	while True:
-		if mode == "+":
-			start += 1
-		elif mode == "-":
-			start -= 1
-		yield start
+
