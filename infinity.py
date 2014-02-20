872aa9dfbdd4674433f3300fc6c5583cd41bac10
Jacob Kodner <jake@kodner.net>
2014-02-20 12:19:52 -0800

    Added Start Option

--- infinity.py
+++ infinity.py
@@ -1,12 +1,12 @@
-def inf(mode="+"):
+def inf(mode="+", start=0):
 	"""Returns a Generator with an infinite amount of positive or negative numbers.
-	Must input '+' to get positive infinity (default) or '-' to get negative infinity."""
+	There are two parameters that one can input. The first is the positive or negative mode +/-.
+	The second is the starting point of the infinity generator."""
 	if mode not in ["+", "-"]:
 		raise ValueError("Input must be '+' or '-'")
-	count = 0
 	while True:
 		if mode == "+":
-			count += 1
+			start += 1
 		elif mode == "-":
-			count -= 1
-		yield count
+			start -= 1
+		yield start
