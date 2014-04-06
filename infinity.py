f81ba70b8605dc800b4e8b6eb7412b74fefa2b3f
Jacob Kodner <jake@kodner.net>
2014-04-06 13:46:43 -0700

    Added 'typ' parameter to 'inf' function'

--- infinity.py
+++ infinity.py
@@ -1,10 +1,28 @@
-def inf(mode="+", start=0, step=1):
+def inf(mode="+", start=0, step=1, typ=False):
 	"""Returns a Generator with an infinite amount of positive or negative numbers.
 	
-	There are three parameters that one can input. 
+	There are four parameters that one can input. 
 	The first is the positive or negative mode +/-.
+	
 	The second is the starting point of the infinity generator.
-	The third is the incremtor (step) for the generator."""
+	
+	The third is the incremtor (step) for the generator.
+	
+	The fourth is a function that changes a number (e.g., float, int) Make sure that
+	the function does not accept two parameters, or else an error will be outputted."""
+	from types import (FunctionType, BuiltinFunctionType, BuiltinMethodType, LambdaType,
+	MethodType)
+	types = (type, FunctionType, BuiltinFunctionType, BuiltinMethodType, LambdaType,
+	MethodType)
+	if not isinstance(typ, types):
+		raise ValueError("'typ' parameter must be function.")
+	if typ == False:
+		def reg(num):
+			return num
+		typ = reg
+	if isinstance(typ, FunctionType):
+		if typ.func_code.co_argcount != 1:
+			raise ValueError("'typ' function must only have 1 parameter")
 	if mode in ["+", "-"]:
 		if isinstance(start, (int, float)) and isinstance(step, (int, float)):
 			while True:
@@ -12,7 +30,7 @@ def inf(mode="+", start=0, step=1):
 					start += step
 				elif mode == "-":
 					start -= step
-				yield start
+				yield typ(start)
 		else:
 			raise ValueError("Start/Step must be integers.")
 	else:
